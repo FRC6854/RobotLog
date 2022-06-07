2e97fb234a84bb97af36777a339246a110cf302c
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2022-06-06 23:14:50 -0400

    Add SCP file transfer library

---
+++ SCPFileTransfer.cpp
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2020-2022 FRC 6854 - Viking Robotics
+
+This file is part of RobotLog.
+
+RobotLog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RobotLog is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RobotLog.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <cstdlib>
+#include <string>
+
+#include "SCPFileTransfer.hpp"
+
+SCPFileTransfer::SCPFileTransfer(const std::string& hostname, const std::string& username)
+	: target_hostname(hostname), target_username(username) {}
+
+std::string SCPFileTransfer::remote_path(const std::string& path) {
+	return target_username + "@" + target_hostname + ":" + path;
+}
+
+bool SCPFileTransfer::ssh_test() {
+	return std::system(("ssh -T " + target_username + "@" + target_hostname).c_str());
+}
+
+int SCPFileTransfer::put(const std::string& local_path, const std::string& target_path) {
+	return std::system(
+		("scp -o ConnectTimeout=3 " + local_path + " " + remote_path(target_path)).c_str());
+}
+
+int SCPFileTransfer::get(const std::string& local_path, const std::string& target_path) {
+	return std::system(
+		("scp -o ConnectTimeout=3 " + remote_path(target_path) + " " + local_path).c_str());
+}
---
+++ SCPFileTransfer.hpp
@@ -0,0 +1,37 @@
+/*
+Copyright (c) 2020-2022 FRC 6854 - Viking Robotics
+
+This file is part of RobotLog.
+
+RobotLog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RobotLog is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RobotLog.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef SCPFILETRANSFER_HPP
+#define SCPFILETRANSFER_HPP
+
+#include <string>
+
+class SCPFileTransfer {
+private:
+	std::string target_hostname, target_username;
+	std::string remote_path(const std::string& path);
+
+public:
+	SCPFileTransfer(const std::string& hostname, const std::string& username);
+	bool ssh_test();
+	int put(const std::string& local_path, const std::string& target_path);
+	int get(const std::string& local_path, const std::string& target_path);
+};
+
+#endif
